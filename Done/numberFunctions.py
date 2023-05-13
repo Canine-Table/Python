e86f344c96ec454b2c1f0ead218cd9a521d3bf69
Canine Table <113630868+Canine-Table@users.noreply.github.com>
2023-05-13 02:45:08 -0400

    4802120000

---
+++ Done/numberFunctions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import math
+pi = 3.14
+print(round(pi)) # regular rounding
+print(math.ceil(pi)) # rounds to the ceiling
+print(math.floor(pi)) # rounds to the floor
+print(abs(pi)) # how far the integer is from 0
+print(math.floor(pow(pi,54))) # power
+print(math.sqrt(math.floor(pow(pi,54)))) # square root
+array = 4,4,43,23,32,2,3,2,34,23,45,2 # a variable with many values
+print(max(array))
+print(min(array))
---
+++ Done/typeCasting.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Type Casting
+# convert the data type if a value to another data type.
+
+x = 1 # Integer
+y = 2.0 # Float
+z = "3" # String
+
+print(float(z)+float(x)+y)
+print(str(float(z)+float(x)+y))
+print(int(float(z)+float(x)+y))
