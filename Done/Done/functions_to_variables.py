13196812aa230be464d50eaec6fbce67bede68de
Canine Table <113630868+Canine-Table@users.noreply.github.com>
2023-05-17 16:14:27 -0400

    45330120000

---
+++ Done/Done/functions_to_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def hello():
+    print('hello world')
+
+hi = hello
+echo = print
+
+hi()
+hello()
+echo('hello world')
---
+++ Done/Done/walrus_operator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# new to Python 3.8
+# assignment expression aka walrus operator
+# assigns values to variables as part of a larger expression
+
+foods = list()
+while food := input('what is your favorite food: ') != 'quit':
+    print(food)
+print(food)
