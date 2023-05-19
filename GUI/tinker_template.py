289328ea1720738bd2beacd1e88f7ab158d989dd
Canine Table <113630868+Canine-Table@users.noreply.github.com>
2023-05-19 12:50:55 -0400

    71033120000

--- GUI/tinker_template.py
+++ GUI/tinker_template.py
@@ -8,12 +8,10 @@
 
 from tkinter import *
 
-
 my_window = Tk() # instantiate an instance of a window
 my_window.geometry('420x420')
 my_window.title("window title")
 my_window.config(background='black')
-my_image = PhotoImage(file='image.png')
-my_window.iconphoto(True,my_image)
+my_window.iconphoto(True,PhotoImage(file='image.png'))
 
 my_window.mainloop() # will place window on computer screen and will listen to events
